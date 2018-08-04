ab22b838cbde459dde7b1c840e0b0a7f65af6c88
weizhenyi <zhenyiwei1986@126.com>
2018-08-04 19:50:48 +0800

    user2 fix Conflicit

--- src/java/net/kafka/producer/Util/ConfigProperties.java
+++ src/java/net/kafka/producer/Util/ConfigProperties.java
@@ -82,4 +82,13 @@ private static final long serialVersionUID = -1851097272122935390L;
 		String zkConnect = config.getProperty("zkconnect");
 	}*/
 
+	public static void sayHello()
+	{
+		system.out.println("user2 add code1");
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("user1 commit code2");
+	}
+
 }
